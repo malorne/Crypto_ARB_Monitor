3702f41ecfb9f2aac6f18771fc7b20769a94e5b8
Timur Rozovel <tdrozovel@gmail.com>
2026-03-01 16:09:00 +0300

    Add engine baseline tests and fixture data

---
+++ tests/test_crypto_arbitrage.cpp
@@ -0,0 +1,48 @@
+#include <gtest/gtest.h>
+#include "crypto/CryptoArbitrageEngine.h"
+#include "exceptions/Exceptions.h"
+#include <fstream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+static std::string tmp_csv(const std::string& body) {
+    auto p = fs::temp_directory_path() / "am_quotes_test.csv";
+    std::ofstream f(p); f << body;
+    return p.string();
+}
+
+TEST(CryptoArbitrage, FindsNetPositiveOpportunityAfterFees) {
+    auto p = tmp_csv(
+        "exchange,symbol,bid,ask,fee_bps\n"
+        "BINANCE,BTCUSD,65450.10,65455.20,8.0\n"
+        "KRAKEN,BTCUSD,65462.40,65468.10,12.0\n");
+    am::CryptoArbitrageEngine eng;
+    auto quotes = eng.load_quotes_csv(p, {}, 10.0);
+    am::CryptoMonitorConfig cfg;
+    auto opps = eng.find_opportunities(quotes, cfg);
+    EXPECT_FALSE(opps.empty());
+    EXPECT_GT(opps.front().net_spread, 0.0);
+}
+
+TEST(CryptoArbitrage, HighFeesRemoveFakeOpportunity) {
+    auto p = tmp_csv(
+        "exchange,symbol,bid,ask,fee_bps\n"
+        "BINANCE,BTCUSD,65450.10,65455.20,500.0\n"
+        "KRAKEN,BTCUSD,65462.40,65468.10,500.0\n");
+    am::CryptoArbitrageEngine eng;
+    auto quotes = eng.load_quotes_csv(p, {}, 10.0);
+    am::CryptoMonitorConfig cfg;
+    EXPECT_TRUE(eng.find_opportunities(quotes, cfg).empty());
+}
+
+TEST(CryptoArbitrage, DoesNotMixDifferentSymbols) {
+    auto p = tmp_csv(
+        "exchange,symbol,bid,ask,fee_bps\n"
+        "BINANCE,BTCUSD,65450.10,65455.20,8.0\n"
+        "KRAKEN,ETHUSD,3500.10,3501.00,10.0\n");
+    am::CryptoArbitrageEngine eng;
+    auto quotes = eng.load_quotes_csv(p, {}, 10.0);
+    am::CryptoMonitorConfig cfg; cfg.symbol_filter = "BTCUSD";
+    EXPECT_TRUE(eng.find_opportunities(quotes, cfg).empty());
+}
